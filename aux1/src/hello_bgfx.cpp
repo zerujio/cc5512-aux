a05a41255f0a8b2db21d74808c6ec8424c780031
Sergio <semorales@ug.uchile.cl>
2022-04-05 09:44:38 -0400

    glfw simple

--- aux1/src/hello_bgfx.cpp
+++ aux1/src/hello_bgfx.cpp
@@ -3,51 +3,15 @@
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 
-#include <stdexcept>
 #include <iostream>
 
-class WindowManager {
-
-public:
-
-    WindowManager(int width, int height, const char* title) :
-    m_ptr(glfwCreateWindow(width, height, title, nullptr, nullptr))
-    {
-        if (!m_ptr)
-            throw std::runtime_error("Window creation failed");
-    }
-
-    ~WindowManager() {
-        if (m_ptr)
-            glfwDestroyWindow(m_ptr);
-    }
-
-    WindowManager(const WindowManager&) = delete;
-
-    WindowManager(WindowManager&& other) : m_ptr(other.m_ptr) {
-        other.m_ptr = nullptr;
-    }
-
-    WindowManager& operator=(WindowManager&& other) {
-        m_ptr = other.m_ptr;
-        other.m_ptr = nullptr;
-        return *this;
-    }
-
-    GLFWwindow* ptr() const {
-        return m_ptr;
-    }
-
-private:
-    GLFWwindow* m_ptr {nullptr};
-
-};
 
 void errorCallback(int code, const char* message) {
     std::cerr << "[GLFW ERROR] CODE=" << code << " MSG='" << message << "'\n";
 }
 
 int main() {
+
     glfwSetErrorCallback(errorCallback);
 
     if (!glfwInit()) {
@@ -55,14 +19,17 @@ int main() {
         return 1;
     }
 
-    {
-    WindowManager window {1024, 768, "Hello bgfx!"};
+    GLFWwindow* window = glfwCreateWindow(1024, 768, "Hello bgfx!", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Window creation failed!" << "\n";
+        return 2;
+    }
 
-    while (!glfwWindowShouldClose(window.ptr())) {
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
-    }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0; 
